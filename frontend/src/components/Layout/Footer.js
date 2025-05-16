1be3753f8e29eef06c85e1f12303a673f813f231
Gangadhar <gangadharp93@gmail.com>
2025-05-16 23:47:21 +0530

    add footer.js

--- frontend/src/components/Layout/Footer.js
+++ frontend/src/components/Layout/Footer.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="footer">
-      <h4 className="text-center">All Right Reserved &copy;</h4>
+      <h4 className="text-center"> &copy; {currentYear} |Gangadhar Pawar| All Rights Reserved </h4>
       <p className="text-center mt-3">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
         <Link to="/policy">Policy</Link>
